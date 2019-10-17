d4002d3606d30ca4d4d921750f2a7ae66252b79d
LD-FRA-1 <ld.fra.1@lightdrops.io>
2019-10-17 15:33:21 +0700

    add store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import allReducers from './reducers';
+import { store } from './store'
 import { Provider } from 'react-redux'
 
-// STORE
-const store = createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
     <Provider store={store}>
